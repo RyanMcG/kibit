68f27142b7870ad3477ec4ad44489fbc2cbade56
Ryan McGowan <ryan@ryanmcg.com>
2012-12-13 23:22:59 -0500

    Crap commit.

--- src/kibit/driver.clj
+++ src/kibit/driver.clj
@@ -2,16 +2,25 @@
   (:require [clojure.tools.namespace :refer [find-clojure-sources-in-dir]]
             [clojure.java.io :as io]
             [kibit.check :refer [check-file]]
-            [kibit.reporters :refer [cli-reporter]]))
+            [kibit.reporters :refer [cli-reporter gfm-reporter]])
+  (:use [clojure.tools.cli :only [cli]]))
+
+(def reporters {"markdown" gfm-reporter
+                "text" cli-reporter})
+
+(def cli-specs [["-r" "--reporter"
+                 "The reporter used when rendering suggestions"
+                 :default "text"]])
 
 (defn run [project & args]
-  (let [source-files (if (empty? args)
+  (let [[options file-args usage-text] (apply (partial cli args) cli-specs)
+        source-files (if (empty? file-args)
                        (mapcat #(-> % io/file find-clojure-sources-in-dir)
                                (or (:source-paths project) [(:source-path project)]))
-                       args)]
+                       file-args)]
     (doseq [file source-files]
       (try (->> (check-file file)
-                (map cli-reporter)
+                (map (reporters (:reporter options)))
                 doall)
            (catch Exception e
              (println "Check failed -- skipping rest of file")
